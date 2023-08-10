9652346c0be0676fad635dd1d1cbd29006b24e43
ya3ya6 <ya3ya63@gmail.com>
2023-08-10 22:49:47 +0430

    c1

--- a.cmd
+++
@@ -1,8 +0,0 @@
-echo console.log('hey') > script.js
-
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/ya3ya6/ktgodaddyweb.git
-git push -u origin main
